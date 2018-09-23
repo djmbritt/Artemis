170ef793913a892a8023266c3f382d0d947d7c8a
ryukishi <donnyschaafs@gmail.com>
2018-09-23 19:32:55 +0200

    0021 Quiz implementatie start. Interface en klassen structuur opgezet.

---
+++ src/main/java/quiz/Quiz.java
@@ -0,0 +1,14 @@
+package quiz;
+
+/**
+ *
+ * @author Donovan Schaafsma
+ *
+ * This interface will provide the component role in the Composite design
+ * pattern. It will include methods for the composite and leafs.
+ */
+public interface Quiz {
+
+    public void questionTypeName(String name);
+
+}
