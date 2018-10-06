27323b6b394a8b15bacf61da6ce9be9772c51179
David Britt <djmb102@gmail.com>
2018-10-06 22:21:54 +0200

    Created and added registration page fxml

---
+++ src/main/resources/com/sbnvw/artemis/controllers/RegistrationPageController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sbnvw.artemis.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author djmbritt
+ */
+public class RegistrationPageController implements Initializable {
+
+    @FXML
+    private Button resetBtnID;
+    @FXML
+    private Button cancelBtnID;
+    @FXML
+    private Button SaveBtnID;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void resetAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancelAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void saveAction(ActionEvent event) {
+    }
+    
+}
