87450efe6be9c247e658bb1d5f9015d88d9fb618
Ryukishi <donnyschaafs@gmail.com>
2018-10-07 17:54:56 +0200

    mediator opzet met methodes {0022}

---
+++ src/main/java/com/sbnvw/artemis/mediator/Mediator.java
@@ -0,0 +1,16 @@
+package com.sbnvw.artemis.mediator;
+
+/**
+ *
+ * @author dono
+ */
+public interface Mediator {
+    
+    /**
+     * We want methods to handle getters and setters that give the desired info
+     * to the desired account. 
+     * So we get the information from the animals and we
+     * give them to the accounts. Some accounts can access certain animals that 
+     * other accounts can't.
+    */
+}
