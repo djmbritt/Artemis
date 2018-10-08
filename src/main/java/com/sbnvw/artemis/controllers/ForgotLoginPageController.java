2143bfb331071e8e99fe52dc5bc648f3e8b40dbd
David Britt <djmb102@gmail.com>
2018-10-08 12:23:46 +0200

    Created forgot_login page and controller.
    Still needs to be implemented

---
+++ src/main/java/com/sbnvw/artemis/controllers/ForgotLoginPageController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sbnvw.artemis.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author djmbritt
+ */
+public class ForgotLoginPageController implements Initializable {
+
+    @FXML
+    private TextField FirstNameID;
+    @FXML
+    private Button cancelBtnID;
+    @FXML
+    private Button SendBtnID;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void cancelAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void SendAction(ActionEvent event) {
+    }
+    
+}
