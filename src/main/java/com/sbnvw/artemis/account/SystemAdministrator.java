ac00dc7a5c0c4f6e22f77b2954d62d6a6373ecd4
David Britt <djmb102@gmail.com>
2018-10-11 17:44:30 +0200

    Refactored SystemAdministrator.java and implemented getinstance()

--- src/main/java/com/sbnvw/artemis/account/SystemAdministrator.java
+++ src/main/java/com/sbnvw/artemis/account/SystemAdministrator.java
@@ -11,22 +11,48 @@ import java.util.Date;
 public class SystemAdministrator extends UserInformation {
 
     /**
-     * Use singleton design pattern to allow the creation of only one instance of the systemAdministrator class.
-     * Using the public static final method
+     * Use singleton design pattern to allow the creation of only one instance
+     * of the systemAdministrator class. Using the public static final method
+     *
      */
-    public static final SystemAdministrator INSTANCE = new SystemAdministrator();
     
-    private SystemAdministrator(){
-    
-        super("sysadmin", "sysadminpass", "System", "Administrator", 
-                new Date(System.currentTimeMillis()), "androgynous", 
-                "sysadmin@artemis.nl", "0205954955", "Wibautstraat", "3", "b", 
-                "1091 GH", null);
-        
-        IOWriter.saveUser(this);
-    
+    /**
+     * Instantiation of System Administrator.
+     */
+    private static final SystemAdministrator INSTANCE = new SystemAdministrator();
+
+    /**
+     * Private Constructor
+     */
+    private SystemAdministrator() {
+
+        super(
+                "sysadmin",                             //User
+                 "sysadminpass",                        //password
+                 "System",                              //FirstName
+                 "Administrator",                       //LastName
+                 new Date(System.currentTimeMillis()),  //DateDateAccount
+                 "androgynous",                         //Sex
+                 "sysadmin@artemis.nl",                 //Email
+                 "0205954955",                          //Telephone
+                 "Wibautstraat",                        //Address
+                 "3",                                   //HouseNumber
+                 "b",                                   //Addition
+                 "1091 GH",                             //PostalCode
+                 null                                   //ProfileImage
+        );
+
+        IOWriter.saveUser(this.INSTANCE);
+
     }
-    
+
+    /**
+     * Sole method to be able to get to SystemAdmin Instance.
+     * @return 
+     */
+    public static SystemAdministrator getInstance() {
+        return INSTANCE;
+    }
 
     /**
      * User Management
@@ -50,17 +76,22 @@ public class SystemAdministrator extends UserInformation {
     public void deleteUser() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
     
+    /**
+     * 
+     * @return String representation of Instance System Administrator
+     */
     @Override
-    public String toString(){
-        
-        return String.format("\n%s\n%s %s\n%s\n%s\n%s\n%s %s%s\n%s\n%s\n%s", 
+    public String toString() {
+
+        return String.format("\n%s\n%s %s\n%s\n%s\n%s\n%s %s%s\n%s\n%s\n%s",
                 super.toString(),
-                "Name:\t\t " + getFirstName() , getLastName(),
+                "Name:\t\t " + getFirstName(), getLastName(),
                 "UserID:\t\t " + getUserID(),
                 "UserName:\t " + getUserName(),
                 "Password:\t " + getUserPassword(),
-                "Address:\t " + getAddres() , getHouseNumber() , getAddition(),
+                "Address:\t " + getAddres(), getHouseNumber(), getAddition(),
                 "PostalCode:\t " + getPostalCode(),
                 "Email:\t\t " + getEmail(),
                 "PhoneNumber:\t " + getPhoneNumber());
