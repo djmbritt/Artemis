dd4d5e538821d6dc5e0cc4927459e95bb1eb7624
Ryukishi <donnyschaafs@gmail.com>
2018-10-11 16:38:42 +0200

    Singleton pattern aan concretemediator toegevoegd {0022}

--- src/main/java/com/sbnvw/artemis/mediator/ConcreteMediator.java
+++ src/main/java/com/sbnvw/artemis/mediator/ConcreteMediator.java
@@ -5,6 +5,24 @@ package com.sbnvw.artemis.mediator;
  * @author Donovan Schaafsma
  */
 public class ConcreteMediator implements Mediator{
+    private static ConcreteMediator concreteMediator = null;
+    
+    private ConcreteMediator(){
+        
+    }
+    
+    private static void createConcreteMediator() {
+        if (concreteMediator == null) {
+            concreteMediator = new ConcreteMediator();
+        }
+    }
+    
+    public static ConcreteMediator getConcreteMediator(){
+        if (concreteMediator == null) {
+            createConcreteMediator();
+        }
+        return concreteMediator;
+    }
 
     @Override
     public void getAnimals() {
