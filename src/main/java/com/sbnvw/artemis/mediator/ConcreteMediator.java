6b93554773f277cf5bda7e27db8208a89ae45adb
ryukishi <donnyschaafs@gmail.com>
2018-10-14 20:11:14 +0200

    mediator udpate ConcreteMediator {0022}
    Added register methods for all of the interacting classes

--- src/main/java/com/sbnvw/artemis/mediator/ConcreteMediator.java
+++ src/main/java/com/sbnvw/artemis/mediator/ConcreteMediator.java
@@ -1,10 +1,42 @@
 package com.sbnvw.artemis.mediator;
 
+import com.sbnvw.artemis.account.Administrator;
+import com.sbnvw.artemis.account.Guest;
+import com.sbnvw.artemis.account.SystemAdministrator;
+import com.sbnvw.artemis.account.User;
+import com.sbnvw.artemis.animal_kingdom.treeOfLife.Animal;
+
 /**
  *
  * @author Donovan Schaafsma
  */
 public class ConcreteMediator implements Mediator{
+    
+    SystemAdministrator sys;
+    Administrator admin;
+    User user;
+    Guest guest;
+    Animal animal;
+    
+    public void registerSystemAdmin (SystemAdministrator sys) {
+        this.sys = sys;
+    }
+    
+    public void registerAdmin (Administrator admin) {
+        this.admin = admin;
+    }
+    
+    public void registerUser (User user) {
+        this.user = user;
+    }
+    
+    public void registerGuest (Guest guest) {
+        this.guest = guest;
+    }
+    
+    public void registerAnimal (Animal animal) {
+        this.animal = animal;
+    }
 
     @Override
     public void getAnimals() {
